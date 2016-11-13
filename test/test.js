eaf064f2722d42a7e8f151aa1ab3e719577a0a53
Andrew Fischer <afischer15@mac.com>
2016-11-13 16:05:30 -0500

    Unit testing

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+describe('Dumb Tests', function() {
+  it('should return true because this is a dumb test', function() {
+    assert.equal(25, 25);
+  });
+  it('should also be true', function() {
+    assert(true);
+  });
+});
