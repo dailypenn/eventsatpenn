fcd2bdfeae56e2474b02e01c2d3ae73d895c742b
Makarios Chung <m.zhong1598@gmail.com>
2017-02-21 20:00:23 -0500

    Added Routing for standin pages

--- app.js
+++ app.js
@@ -16,10 +16,33 @@ var user = {
   email: "email"
 }
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { 
       res.render('index.jinja', {foo: user});
 });
 
+app.get('/calendar', function(req, res) {
+      res.render('calendar.jinja', {foo: user});
+});
 
-app.listen(process.env.PORT || 8000);
+app.get('/organization/new', function(req, res) {
+      res.render('organization/new.jinja', {foo: user});
+});
+
+app.get('/organization/view', function(req, res) {
+      res.render('organization/view.jinja', {foo: user});
+});
+
+app.get('/organization/:id', function(req, res) {
+      res.render('organization/view.jinja', {foo: user, id: req.params['id']})
+})
+
+app.get('/events/new', function(req, res) {
+      res.render('events/new.jinja', {foo: user});
+});
+
+app.get('/events/view', function(req, res) {
+      res.render('events/view.jinja', {foo: user});
+});
+
+app.listen(process.env.PORT || 8000); //starts up the server
 console.log('server is running');
