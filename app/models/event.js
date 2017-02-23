9345b0f607871fc0fe1454285bcd2d221a9fc7ad
Andrew Fischer <afischer15@mac.com>
2017-02-23 10:18:02 -0500

    Enum config, event config file, config reorg

--- app/models/event.js
+++ app/models/event.js
@@ -1,15 +1,16 @@
 "use strict";
 module.exports = function(sequelize, DataTypes) {
+  var config       = require(__dirname + '/../config/eventConfig.js');
   var PlannedEvent = sequelize.define("Event", {
     title: DataTypes.STRING,
     start_time: DataTypes.DATE,
     end_time: DataTypes.DATE,
     description: DataTypes.TEXT,
     location: DataTypes.TEXT,
-    category: DataTypes.ENUM,
+    category: DataTypes.ENUM(config.categories),
     twentyoneplus: DataTypes.BOOLEAN,
     recurring: DataTypes.BOOLEAN,
-    recurrence_freq: DataTypes.ENUM,
+    recurrence_freq: DataTypes.ENUM(config.recFreqs),
     recurrence_amt: DataTypes.INTEGER,
   });
 
