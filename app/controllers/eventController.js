557452ac19aba1a49a050f7ef376601374269060
Andrew Fischer <afischer15@mac.com>
2017-03-30 11:38:43 -0400

    events → ejs

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -1,22 +1,18 @@
 module.exports = function(app){
   var PlannedEvent = require('../models').Event;
 
-  app.get('/org/:org_id/event', function(req, res) {
-    PlannedEvent.sync();
-    PlannedEvent.findAll().then(function(events) {
-      res.render('events/all.jinja', {events: events});
-    })
-  });
+  // app.get('/org/:org_id/event', function(req, res) {
+  //   PlannedEvent.sync();
+  //   PlannedEvent.findAll().then(function(events) {
+  //     res.render('events/all.jinja', {events: events});
+  //   })
+  // });
 
   app.get('/org/:org_id/event/new', function(req, res) {
-    res.render('events/new.jinja', {});
-  });
-
-  app.get('/org/:org_id/event/view', function(req, res) {
-    res.render('events/view.jinja', {});
+    res.render('events/new', {});
   });
 
   app.get('/org/:org_id/event/:id', function(req, res) {
-        res.render('events/view.jinja', {id: req.params['id']})
+        res.render('events/view', {id: req.params['id']})
   })
 };
