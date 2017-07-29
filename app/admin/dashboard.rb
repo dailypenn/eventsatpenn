55db3c877194c3e995612eb57bf78e310e8012b6
Andrew Fischer <afischer15@mac.com>
2017-07-29 15:11:38 -0400

    clean up comments

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,15 +1,7 @@
-ActiveAdmin.register_page "Dashboard" do
+ActiveAdmin.register_page 'Dashboard' do
+  menu priority: 1, label: 'Dashboard'
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    # div class: "blank_slate_container", id: "dashboard_default_message" do
-    #   span class: "blank_slate" do
-    #     span I18n.t("active_admin.dashboard_welcome.welcome")
-    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #   end
-    # end
-
+  content title: proc{ I18n.t('active_admin.dashboard') } do
     columns do
       column do
         panel 'Recently Created Accounts' do
@@ -33,9 +25,9 @@ ActiveAdmin.register_page "Dashboard" do
 
       column do
         panel 'Info' do
-          para "Welcome to Events@Penn admin"
+          para 'Welcome to Events@Penn admin'
         end
       end
     end
-  end # content
+  end
 end
