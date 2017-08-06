0b77a32ae207996bc9b63d16b607c41b3061faff
Alex Graves <graves@thedp.com>
2017-08-06 13:31:20 -0700

    add buttons to add events/orgs on list all pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723204830) do
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.string "author_type"
-    t.integer "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
+ActiveRecord::Schema.define(version: 20170529180117) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -89,7 +75,7 @@ ActiveRecord::Schema.define(version: 20170723204830) do
     t.string "last_name"
     t.string "email"
     t.string "image_url"
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
