2d14af157443ba58ecbf9019c2c24db1fb75155b
Andrew Fischer <afischer15@mac.com>
2017-08-20 17:46:11 -0400

    Add sentry dsn

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-require 'raven'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,10 +7,6 @@ Bundler.require(*Rails.groups)
 
 module Eventsatpenn
   class Application < Rails::Application
-
-    Raven.configure do |config|
-      config.dsn = ENV['SENTRY_DSN']
-    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
@@ -21,5 +16,9 @@ module Eventsatpenn
 
     # Set calendar first-day of week to Sunday, since this 'Murica
     config.beginning_of_week = :sunday
+
+    Raven.configure do |config|
+      config.dsn = 'https://bc380f8287694c2d9a3597b374e6dd07:27a8dc2c1cea49d3af537d6a49778394@sentry.io/205950'
+    end
   end
 end
