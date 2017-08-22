a253849114f4601fb22db20ab8e10425f486a954
Andrew Fischer <afischer15@mac.com>
2017-08-22 14:19:24 -0400

    Add additional nil protection in activejob

--- app/jobs/scrape_new_events_job.rb
+++ app/jobs/scrape_new_events_job.rb
@@ -5,7 +5,7 @@ class ScrapeNewEventsJob < ApplicationJob
     group = FbGraph2::Page.new(org.fbID).authenticate(access_token)
     added_events = 0
     group.events.each do |event|
-      evt = event.fetch(fields: 'name, category, description, place, start_time, end_time, id')
+      evt = event.fetch(fields: 'name,category,description,place,start_time,end_time,id')
       if Event.find_by(fbID: evt.id).nil?
         create_event_for_org(org, evt)
         added_events += 1
@@ -14,13 +14,14 @@ class ScrapeNewEventsJob < ApplicationJob
   end
 
   def create_event_for_org(org, event)
+    return if event.nil?
     place = event.raw_attributes['place']
     new_event = Event.new(
       title: event.name, start_date: event.start_time,
       category: event.raw_attributes['category'],
       end_date: event.end_time, description: event.description,
       all_day: false, recurring: false,
-      location: "#{place['name']}, #{place['location']['street']}",
+      location: place.nil? ? nil : "#{place['name'] unless place['name'].nil?}, #{place['location']['street'] unless place['location'].nil?}",
       location_lat: place['location']['latitude'],
       location_lon: place['location']['longitude'], fbID: event.id
     )
