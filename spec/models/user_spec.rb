532d85154286aae05b5558294e616212a6941153
Andrew Fischer <afischer15@mac.com>
2017-08-24 18:58:03 -0400

    Simple user test with fixture

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  fixtures :users
+
+  describe '#admin?' do
+    it 'returns false when user is not admin' do
+      snow = users(:john_snow)
+      result = snow.admin?
+      expect(result).to eq false
+    end
+
+    it 'returns true when user is admin' do
+      cerc = users(:cersei_lannister)
+      result = cerc.admin?
+      expect(result).to eq true
+    end
+  end
+
   pending "add some examples to (or delete) #{__FILE__}"
 end
