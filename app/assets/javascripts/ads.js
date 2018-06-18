a8f1806a6ba9b244658fd87aeff4d521106b869c
Alex Graves <graves@thedp.com>
2018-06-17 17:45:33 -0700

    fix ads by switching google dfp, closes #95

---
+++ app/assets/javascripts/ads.js
@@ -0,0 +1,5 @@
+document.addEventListener('turbolinks:load', () => {
+  googletag.cmd.push(() => googletag.display('div-gpt-ad-1529280717739-0'));
+  googletag.cmd.push(() => googletag.display('div-gpt-ad-1529280717739-1'));
+  googletag.cmd.push(() => googletag.display('div-gpt-ad-1529280717739-2'));
+})
